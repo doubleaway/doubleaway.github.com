bbd93bf08d96aafebf6d55c592ae4f740fdadf98
DESKTOP-92SISGK\yangcharin <ycl042138@gmail.com>
2021-12-14 14:14:39 +0900

    페이지 나누기

--- blog_project/src/App.js
+++ blog_project/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Route, Routes} from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import PostList from "./pages/PostList";
+import PostPage from "./pages/PostPage";
+import RegisterPage from "./pages/RegisterPage";
+import WritePage from "./pages/WritePage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+       <Routes>
+         <Route component={PostList} path={"/"} exact/>
+         <Route component={LoginPage} path="/login"/>
+         <Route component={PostPage} path="/register"/>
+         <Route component={RegisterPage} path="/write"/>
+         <Route component={WritePage} path="/@:username/:postid"/>
+       </Routes>
+   </>
   );
 }
 
---
+++ blog_project/src/pages/WritePage.js
@@ -0,0 +1,6 @@
+import React from "react";
+const WritePage=()=>{
+    return <div>WirtePage</div>
+}
+
+export default WritePage
