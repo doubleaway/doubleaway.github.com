185d1959093bbccb08fbf1c809e066489890bcbc
DESKTOP-92SISGK\yangcharin <ycl042138@gmail.com>
2022-01-06 23:44:35 +0900

    [메모]web용 알림창 css수정

--- note_app/src/component/Notification.js
+++ note_app/src/component/Notification.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "./button";
 import {IoIosCreate} from "react-icons/io";
-import { AiFillCloseCircle,AiOutlineDelete } from "react-icons/ai";
+import { AiFillCloseCircle,AiFillAlert } from "react-icons/ai";
 import { GrTrash } from "react-icons/gr";
 
 const Notification=()=>{
@@ -11,7 +11,7 @@ const Notification=()=>{
             <aside >
                 <Button contents={<AiFillCloseCircle color="white" size="60px"/>} color="light_gray" classN="notification" contentsText=""></Button>
                 <div className="notification_icon">
-                    <AiOutlineDelete color="white"/>
+                    <AiFillAlert color="white"/>
                     <h1>메모가 삭제됩니다.</h1>
                 </div>
                 <div className="notification_content"><p>다시 복구할 수 없습니다.</p><p>정말 삭제하시겠습니까? </p></div>
