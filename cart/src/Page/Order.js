8737740bdfb4e0b13d2871518dfb7b1a604a0e28
DESKTOP-92SISGK\yangcharin <ycl042138@gmail.com>
2022-02-03 12:35:48 +0900

    [cart] 사용할 이미지 (후드티14개)/ style기본 파일 옮기기/ app.js삭제/ 주문 페이지 기본으로 띄워놓기

---
+++ cart/src/Page/Order.js
@@ -0,0 +1,17 @@
+import React from "react";
+const Order=()=>{
+return(
+    <>
+        <div>
+            주문페이지
+
+        </div>
+        <a>d</a>
+    </>
+)
+    
+    
+}
+
+
+export default Order;
