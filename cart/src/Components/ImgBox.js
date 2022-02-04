ac10f1540c998f1512fd5283bf9dc0404c98ac24
DESKTOP-92SISGK\yangcharin <ycl042138@gmail.com>
2022-02-04 12:49:51 +0900

    [카트] 리액트 아이콘설치/ 설명 ui및 아이템 넣기/ 아래 이미지 누르면 메인이미지 변하게 시도 중..

--- cart/src/Components/ImgBox.js
+++ cart/src/Components/ImgBox.js
@@ -1,20 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 const ImgBox=({classN,color,number})=>{
-
+    const [imgChange,setImgChange]=useState('');
     let imgsrc=[];
     for(var i=0; i<Number(number);i++){
         imgsrc.push(i+1);
     }
-    console.log(imgsrc);
     const srcO="img/"+color+'cloth (';
     const srcE=").png";
     return(
         <div className={classN}>
             {
                 imgsrc.map(img=>(
-                    <img key={img} src={srcO+img+srcE}/>
+                    <img key={img} src={srcO+img+srcE} onClick={}/>
                 ))
             }
         </div>
--- cart/src/Page/Order.js
+++ cart/src/Page/Order.js
@@ -1,23 +1,47 @@
-import React from "react";
-import ImgBox from "../Components/ImgBox";
+import React, {useState} from "react";
+// import ImgBox from "../Components/ImgBox";
 import Button from "../Components/button";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
 
 const Order=()=>{
-
-
 
 return(
     <section >
         <header>
             <h1>The Palete</h1>
+            <span>포트폴리오용으로 작업된 사이트입니다.</span>
             <Button color="none" contents="장바구니"/>
+
         </header>
         <div className="body_box">
            <div className="pic_box">
-               <ImgBox number="1" classN="main" color="black"/>
+               <ImgBox number="1" classN="main" color="black" mainImg="1"/>
                 <ImgBox number="4" classN="sub" color="black"/>
            </div>
-           <div className="ex_box">2</div>
+           <div className="ex_box">
+               <h1>
+                   상품명
+               </h1>
+               <hr/>
+               <div className="price_box">
+                   <h3>Price</h3>
+                   <h2>65,000</h2>
+               </div>
+               <hr/>
+               <div className="info_box">
+                   <h1><span><AiOutlineShoppingCart/> </span> 배송비 <span>5000원</span></h1>
+                   <p>10만원이상 구매 시 <span className="axent"> 무료 배송!</span></p>
+                   <p ><span className="axent">오늘 주문 시 이틀 뒤 도착예정</span>(지역에 따라 배송이 지연될 수 있습니다. 경기/서울 기준)</p>               </div>
+               <hr/>
+               <div className="color_change_button_box">
+                   <h3>Color</h3>
+
+                   <Button color="red" contents="" classN="color_change"/>
+                   <Button color="gray" contents="" classN="color_change"/>
+                   <Button color="black" contents="" classN="color_change"/>
+               </div>
+           </div>
            <div className="select_box">3</div>
         </div>
     </section>
@@ -25,6 +49,26 @@ return(
     
     
 }
+const ImgBox=({classN,color,number,mainImg})=>{
+    const [imgChange,setImgChange]=useState(mainImg);
+    var mainSrc="";
+    const imgChangeFunc=(num)=>{
+        setImgChange(num);
+        console.log(mainSrc);
+    }
+    mainSrc="img/"+color+"cloth ("+imgChange+").png";
+    let imgsrc=[];
+    for(var i=0; i<Number(number);i++){
+        imgsrc.push(i+1);
+    }
+    const srcO="img/"+color+'cloth (';
+    const srcE=").png";
+    const mainImgBox="<img src="+mainSrc+"/>";
+    return(
+        classN=="main"?{mainImgBox}:<div className="sub">{imgsrc.map(img=>(<img key={img} src={srcO+img+srcE} onClick={()=>imgChangeFunc(img)}/>))}</div>
+    )
+}
+
 
 
 export default Order;
