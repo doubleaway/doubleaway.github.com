a9b4b5fbe9982af2bbf6e3ce7a7e3ea4d2b94ec1
DESKTOP-92SISGK\yangcharin <ycl042138@gmail.com>
2022-02-23 20:17:40 +0900

    [cart] spa준비

--- cart/src/Conatiners/OrderContainer.js
+++ cart/src/Conatiners/OrderContainer.js
@@ -1,15 +1,15 @@
-import {useCallback} from "react";
-import {counterChange,remove} from "../Modules/cart";
-import {useDispatch, useSelector} from "react-redux";
-import ShoppingBasket from "../Page/ShoppingBasket";
-const selectCont=useSelector(state=>state.cart);
-const dispatch=useDispatch();
+// import {useCallback} from "react";
+// import {counterChange,remove} from "../Modules/cart";
+// import {useDispatch, useSelector} from "react-redux";
+// import ShoppingBasket from "../Page/ShoppingBasket";
+// const selectCont=useSelector(state=>state.cart);
+// const dispatch=useDispatch();
+// // const onAdd=useCallback(color,size=>dispatch(add(color,size)),[dispatch]);
 // const onAdd=useCallback(color,size=>dispatch(add(color,size)),[dispatch]);
-const onAdd=useCallback(color,size=>dispatch(add(color,size)),[dispatch]);
-const OrderContainer=()=> {
-    return(
-    <ShoppingBasket selectCount={selectCont} onAdd={onAdd}/>
-    )
-}
+// const OrderContainer=()=> {
+//     return(
+//     <ShoppingBasket selectCount={selectCont} onAdd={onAdd}/>
+//     )
+// }
 
-export default OrderContainer;
+// export default OrderContainer;
